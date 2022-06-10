076dc113a40a09e56f27842a89ae5a22d0539da1
KevinBaoB <kevinbaobui@gmail.com>
2022-06-10 08:48:57 -0500

    First

--- classes/school.py
+++ classes/school.py
@@ -12,7 +12,14 @@ class School:
         for i, student in enumerate(self.students):
             print(f'{i + 1}. {student.name} {student.school_id}')
 
+    def add_student(self, student_data):
+        self.students.append(student_data)
+
+    def remove_student(self, student_to_remove):
+        self.students.remove(self.find_student_by_id(student_to_remove))
+
     def find_student_by_id(self, student_id):
         for student in self.students:
             if student.school_id == student_id:
                 return student
+
--- runner.py
+++ runner.py
@@ -1,4 +1,5 @@
 from classes.school import School 
+from classes.student import Student
 
 school = School('Ridgemont High') 
 
@@ -7,9 +8,24 @@ while True:
 
     if mode == '1':
         school.list_students()
+        print(school.students)
     elif mode == '2':
         student_id = input('Enter student id:')
         student_string = str(school.find_student_by_id(student_id))
         print(student_string)
+    elif mode == '3':
+        student_data = {'role':'student'}
+        student_data['name']      = input('Enter student name:\n')
+        student_data['age']       = input('Enter student age: \n')
+        student_data['school_id'] = input('Enter student school id: \n')
+        student_data['password']  = input('Enter student password: \n')
+    
+        school.add_student(Student(**student_data))
+
+    elif mode == "4":
+        student_to_remove = input('Enter student school ID:\n')
+
+        school.remove_student(student_to_remove)
+    
     elif mode == '5':
         break
